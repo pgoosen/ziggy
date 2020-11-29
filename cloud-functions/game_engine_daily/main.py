09044e1f3d95ff0c64f13db458416401488463ad
Anri <8684745+GoosenA@users.noreply.github.com>
2020-11-29 21:30:06 +0200

    Add game_engine_daily cloud function

---
+++ cloud-functions/game_engine_daily/main.py
@@ -0,0 +1,259 @@
+import json
+import time
+import base64
+import os
+
+from google.cloud import firestore
+from google.cloud import pubsub_v1
+
+from flask import jsonify
+from datetime import datetime
+
+from game_engine.transaction import Transaction
+from game_engine.avatar import Avatar
+from game_engine.goal import Goal
+from game_engine import lookups
+
+
+def ProcessDailyTasks(event, context):
+    """
+    Process daily tasks triggered by Cloud SCheduler event on PubSub
+    """
+    decoded = base64.b64decode(event["data"]).decode("utf-8")
+    task = json.loads(decoded)
+
+    if "avatar_daily_health" in task["task"]:
+        update_avatar_health()
+    if "process_daily_transactions" in task["task"]:
+        process_daily_transactions()
+    if "goal_daily_processing" in task["task"]:
+        process_goals_daily()
+
+    return "ok"
+
+
+def update_avatar_health():
+    """
+    Increase avatar health daily
+    """
+    avatar = get_avatar()
+    level = avatar.level
+    try:
+        hp_impact = avatar.increase_hp()
+        level_diff = avatar.level-level 
+        update_avatar(avatar)
+        send_avatar_notification(avatar, hp_impact, 0, level_diff)
+    except Exception as exc:
+        print(exc)
+    
+
+def get_avatar():
+    """
+    Get the avatar from the database
+    """
+    db = firestore.Client()
+    avatar_ref = db.collection(u'avatar')
+    docs = avatar_ref.stream()
+
+    result = {}
+    for doc in docs:
+        _doc = doc.to_dict()
+        result[doc.id] = _doc
+
+    avatar_id = list(result.keys())[0]
+    avatar = Avatar(config=result[avatar_id], avatar_id=avatar_id)
+    return avatar
+
+
+def update_firebase_entry(collection, docId, data):
+    """
+    Update a document in firestore
+    """
+    db = firestore.Client()
+    db.collection(collection).document(docId).update(data)
+
+
+def update_avatar(avatar):   
+    """
+    Update the avatar document in the database
+    """
+    updated_info = {
+        "current_hp": avatar.current_hp,
+        "current_xp": avatar.current_xp,
+        "level": avatar.level
+    }
+    update_firebase_entry(u'avatar', avatar._id, updated_info)
+
+
+def process_goals_daily():
+    """
+    Process daily goals related tasks:
+        Activate/Deactivate goals based on their start dates and end dates
+        Determine if any goals have been achieved and process the impact thereof on the avatar
+    """
+    goals = get_goals(active=False)
+    activate_goals(goals)
+
+    goals = get_goals(active=False)
+    avatar = get_avatar()
+    process_goal_achievements(goals, avatar)
+
+
+def get_goals(active=True):
+    """
+    Get all the goals from the database
+    """
+    db = firestore.Client()
+    goals_ref = db.collection(u'goals')
+    docs = goals_ref.stream()
+    
+    result = {}
+    for doc in docs:
+        _doc = doc.to_dict()
+        if active:
+            if _doc["active"]:
+                result[doc.id] = _doc
+        else:
+            result[doc.id] = _doc
+    return result
+
+
+def activate_goals(goals):
+    """
+    Activate goals based on their start date. Deactviate goals based on their end date.
+    """
+    for goal_id, goal_value in goals.items():
+        goal = Goal(config=goal_value, doc_id=goal_id)
+        if goal.start_date and goal.end_date and datetime.strptime(goal.start_date, "%Y-%m-%d") < datetime.now() and datetime.strptime(goal.end_date, "%Y-%m-%d") >= datetime.now():
+            goal.active=True
+            set_goal_active(goal_id=goal_id)
+        elif not goal.end_date and goal.start_date and datetime.strptime(goal.start_date, "%Y-%m-%d") < datetime.now():
+            goal.active=True
+            set_goal_active(goal_id=goal_id)
+        elif not goal.start_date and goal.end_date and datetime.strptime(goal.end_date, "%Y-%m-%d") >= datetime.now():
+            goal.active=True
+            set_goal_active(goal_id=goal_id)
+        else:
+            goal.active=False
+            set_goal_active(goal_id=goal_id, active=False)
+
+
+def set_goal_active(goal_id=None, active=True):
+    """
+    Save the updated goals state to the database
+    """
+    updated_info = {
+        "active": active
+    }
+    if goal_id:
+        db = firestore.Client()
+        db.collection("goals").document(goal_id).update(updated_info)
+
+
+def process_goal_achievements(goals, avatar):
+    """
+    Determine if any goals have been achieved or not and process teh impact thereof on the avatar.
+    """
+    hp_impact = 0
+    xp_impact = 0
+    avatar_start_level = avatar.level
+
+    for goal_id, goal_value in goals.items():
+        try:
+            goal = Goal(config=goal_value, doc_id=goal_id)
+            if not goal.active and not goal.complete and goal.end_date and datetime.strptime(goal.end_date, "%Y-%m-%d") < datetime.now():
+                if goal.goal_type == lookups.GoalType.Spending.value:
+                    if goal.goal_details["spending_type"] == lookups.SpendingType.TotalValue.value:
+                        progress_value = int(goal.goal_details.get("progress_value", 0))
+                        value_limit = int(goal.goal_details.get("value_limit", -1))
+                        if value_limit == -1:
+                            # do nothing
+                            continue
+                        elif progress_value > value_limit: 
+                            hp_impact += avatar.decrease_hp(hp_decrease=2)
+                            xp_impact += avatar.decrease_xp(xp_decrease=2)
+                        else:
+                            xp_impact += avatar.increase_xp(xp_increase=3)
+                    elif goal.goal_details["spending_type"] == lookups.SpendingType.NumberTransactions.value: 
+                        progress_count = int(goal.goal_details.get("progress_count", 0))
+                        count_limit = int(goal.goal_details.get("count_limit", -1))
+                        if count_limit == -1:
+                            # do nothing
+                            continue
+                        elif progress_count > count_limit:
+                            hp_impact += avatar.decrease_hp(hp_decrease=2)
+                            xp_impact += avatar.decrease_xp(xp_decrease=2)
+                        else:
+                            xp_impact += avatar.increase_xp(xp_increase=3)
+
+                elif goal.goal_type == lookups.GoalType.Savings.value:
+                    progress_value = int(goal.goal_details.get("progress_value", 0))
+                    value_limit = int(goal.goal_details.get("value_limit", -1))
+                    if value_limit == -1:
+                        # do nothing
+                        continue
+                    elif progress_value < value_limit:
+                        hp_impact += avatar.decrease_hp(hp_decrease=2)
+                        xp_impact += avatar.decrease_xp(xp_decrease=2)
+                    else:
+                        xp_impact += avatar.increase_xp(xp_increase=3)
+                
+                goal.complete = True
+                        
+                update_avatar(avatar)
+                update_goal(goal, goal_id)
+        except Exception as exc:
+            print(exc)
+        
+    avatar_level_diff = avatar.level - avatar_start_level
+    send_avatar_notification(avatar, hp_impact, xp_impact, avatar_level_diff)
+
+
+def update_goal(goal, goal_id):
+    """
+    If a finished goal has been completed and processed, update the goals document in the database
+    """
+    updated_info = {
+        "complete": goal.complete,
+    }
+    if goal_id:
+        db = firestore.Client()
+        db.collection("goals").document(goal_id).update(updated_info)
+
+
+def send_avatar_notification(avatar, hp_impact=0, xp_impact=0, level_diff=0):
+    """
+    Send an avatar update slack notification
+    """
+    if hp_impact == 0 and xp_impact == 0 and level_diff == 0:
+        return
+
+    project_id = os.environ.get("PROJECT_ID", None)
+    topic_id = os.environ.get("TOPIC_ID", None)
+
+    if not project_id or not topic_id:
+        print("Unable to post to PubSub. No project_id/topic_id set.")
+        return
+    
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    payload = { 
+        "type": "avatar_update",
+        "content": {
+            "current_hp": avatar.current_hp,
+            "current_xp": avatar.current_xp,
+            "level": avatar.level,
+            "xp_diff": xp_impact,
+            "hp_diff": hp_impact,
+            "level_diff": level_diff
+        }
+    }
+
+    data = json.dumps(payload).encode("utf-8")
+    publisher.publish(topic_path, data)
+
+
+def process_daily_transactions():
+    # TODO: Implement get daily transactions
+    pass
